bd23e32156d93470fa463ff7bc8b6359dba6f00d
meghdadFar <meghdad.farahmand@gmail.com>
2020-08-24 09:27:34 +0200

    Create dummy state and reward functions

--- rl_nlp/dialouge_manager/main.py
+++ rl_nlp/dialouge_manager/main.py
@@ -1,22 +1,26 @@
 from rl_nlp.steps import RLStep
 from rl_nlp.qtable import QTable
 from rl_nlp.Q_learning import QL
-
+import random
 
 class MyRlStep(RLStep):
     
     def get_state(self):
-        NotImplementedError
+        state = []
+        state.append(random.randint(0,2))
+        state.append(random.randint(0,3))
+        state.append(random.randint(0,3))
+        return state
     
     def get_reward(self):
-        NotImplementedError
+        return random.randint(1,5)
 
 
 if __name__ == "__main__":
 
     mrls = MyRlStep()
     state_space=[2, 3, 3]
-    actions = ['a', 'b', 'c']
+    actions = ['bert_salutation', 'bert_farewell', 'bert_generic', 'bert_mean']
 
     qt = QTable(state_space=state_space, actions=actions)
     ql = QL(qtable=qt, rl_step=mrls)
