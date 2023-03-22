d4ef8ab51cc3da22a2938d265488d8d01dead562
meghdadFar <meghdad.farahmand@gmail.com>
2023-03-22 17:01:04 +0100

    Update qtable docstring

--- kandula/qtable.py
+++ kandula/qtable.py
@@ -4,25 +4,36 @@ from functools import reduce
 import itertools
 
 class QTable:
+    """
+    Provides a number of analysis functionalities --such as class distribution, for labels.
 
+    Attributes:
+        state_space: A list of integers. Each index of the list represents one dimension of the state space and the value
+            at that index represents the number of possible values for that dimension.
+        actions: A list of possible actions that the RL agent is allowed to take. 
+        q_table: Qtable.
+        state_index: 
+        action_index:
+
+    Example:
+        >>> state_space=[5, 5]
+        >>> actions = [i for i in range(1,26)]
+        >>> qt = QTable(state_space=state_space, actions=actions)
+    """
     def __init__(self, state_space, actions):
-        """
+        """Initialize a QTable object by defining attributes by creating a Qtable from states and actions.
 
        Args:
             state_space (list): A list of integers. Each index of the list represents one dimension of the state space and the value
-            at that index represents the number of possible values for that dimension. For instance, if the first index
-            represents a 5 class concept, the value at this index should be 5. 
-            
+                at that index represents the number of possible values for that dimension. For instance, if the first index
+                represents a 5 class concept, the value at this index should be 5. 
             actions (list): A list of possible actions that the RL agent is allowed to take. 
 
         Returns:
-
+            None
         """
-
         self.state_space = state_space
         self.actions = actions
-
-        logger.info('Creating Q-table')
         num_states = reduce(lambda x, y: x*y, state_space)
         num_actions = len(actions)
         self.q_table = torch.zeros([num_states, num_actions])
@@ -40,7 +51,7 @@ class QTable:
         """
         # Create all combinations
         elements = [[i for i in range(1, l+1)] for l in self.state_space]
-        all_possible_states = list(itertools.product(*elements)) # for state_space [2, 3, 3]], res looks like: [(1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), (1, 2, 3), (1, 3, 1), ...]
+        all_possible_states = list(itertools.product(*elements)) # for state_space [2, 3, 3]], `all_possible_states` looks like: [(1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), (1, 2, 3), (1, 3, 1), .., (5, 5, 5)]
 
         state_index_dict = {}
         index_state_dict = {}
