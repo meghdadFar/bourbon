471b56ac97347686d7b36c94cd30d91816c7df58
meghdadFar <meghdad.farahmand@gmail.com>
2023-04-27 17:05:34 +0200

    Rm whitespace

--- kandula/Q_learning.py
+++ kandula/Q_learning.py
@@ -5,8 +5,6 @@ from random import randint
 from typing import Type
 
 
-
-
 class Egreedy:
     """Implements parameters of E-greedy algorithm."""
     def __init__(self,
@@ -50,14 +48,12 @@ class QL:
         Returns:
             None
         """
-
         self.q_table= qtable
         self.rl_step = rl_step
         self.gamma = gamma
         self.alpha = alpha
         self.egreedy = Egreedy(egreedy_first, egreedy_decay, egreedy_last)
 
-    
     def _random_action(self, n_actions: int):
         """Select a random action from among actions in the input
         
@@ -69,7 +65,6 @@ class QL:
         """
         return randint(0, n_actions-1)
 
-
     def _select_train_action(self, state_index):
         """Helper function to selection a train action based on the adaptive epsilon greedy mechanism.
         
@@ -78,7 +73,6 @@ class QL:
 
         Returns:
             action
-
         """
         # Select action based on the adaptive epsilon greedy mechanism
         if torch.rand(1)[0] > self.egreedy.egreedy_first:
@@ -91,7 +85,6 @@ class QL:
             action = self._random_action(len(self.q_table.actions))  # Index of the best action
         return action
 
-
     def train(self):
         """Trains the RL system by updating the Q-table via Q-learning rule.
         
@@ -103,21 +96,17 @@ class QL:
         """
         # Get the current state
         state = self.rl_step.get_state()
-
         # Convert state to index
         state_index = self.q_table.get_state_index(state)
-
         # Reinforcement Learning Core Algorithm in 4 steps
         # 1. Select Action
         action_index = self._select_train_action(state_index)
         if self.egreedy.egreedy_first > self.egreedy.egreedy_last:
             self.egreedy.egreedy_first *= self.egreedy.egreedy_decay
-
         # 2. Get reward and the new state
         reward = self.rl_step.get_reward(state, self.q_table.actions[action_index])
         new_state = self.rl_step.get_state()
         new_state_index = self.q_table.get_state_index(new_state)
-
         # 3. Update Q-table via Q-learning rule: Q[s,a] + α(r + γmaxa' Q[s',a'] - Q[s,a])
         q_sa = self.q_table.q_table[state_index, action_index]
         q_spap = torch.max(self.q_table.q_table[new_state_index])
