c91b5508659a88c014b4f1f028d6b34fae4b4346
meghdadFar <meghdad.farahmand@gmail.com>
2023-05-03 14:31:15 +0200

    Fix state index issue

--- kandula/capitals.py
+++ kandula/capitals.py
@@ -20,10 +20,12 @@ with open("resources/country_capital.json", "r") as fc:
     capitals: List = json.load(fc)
 capitals_dict = {}
 country_index = {}
-i=0
-for jl in capitals.items():
+index_country = {}
+i=1
+for jl in capitals:
     capitals_dict[jl["country"]] = ["capital"]
     country_index[jl["country"]] = i
+    index_country[i] = jl["country"]
     i+=1
 
 
@@ -51,7 +53,9 @@ class MyRlStep(RLStep):
         return state
     
     def get_reward(self, state, action):
-        reward = [1 if capitals_dict[state] == action else 0]
+        # print(state)
+        s = reduce((lambda x: x), state)
+        reward = 1 if index_country[s] == action else 0
         return reward
 
 
@@ -80,45 +84,45 @@ if __name__ == "__main__":
     logging.info('Creating required objects')
     mrls = MyRlStep()
     state_space = [248]
-    actions = [v for _, v in capitals.items()]
+    actions = [v for _, v in capitals_dict.items()]
 
     qt = QTable(state_space=state_space, actions=actions)
     ql = QL(qtable=qt, rl_step=mrls)
 
-    # logging.info('Initializing plot...')
-    # viz = visdom.Visdom()
-    # win = viz.line(
-    #     X=np.array([0]), Y=np.array([0]))
+    logging.info('Initializing plot...')
+    viz = visdom.Visdom()
+    win = viz.line(
+        X=np.array([0]), Y=np.array([0]))
 
-    # logging.info('Training the model...')
-    # num_epochs = 70000
-    # for e in range(1, num_epochs):
-    #     q_table = ql.train()
-    #     if e % 1000 == 0:
-    #         eval_results = evaluate_my_rl_agent(state_space, actions, q_table)
-    #         viz.line(
-    #             X=np.array([e]),
-    #             Y=np.array([eval_results]),
-    #             win=win,
-    #             name='Error',
-    #             update='append')
-    #         time.sleep(0.1)
+    logging.info('Training the model...')
+    num_epochs = 70000
+    for e in range(1, num_epochs):
+        q_table = ql.train()
+        if e % 1000 == 0:
+            eval_results = evaluate_my_rl_agent(state_space, actions, q_table)
+            viz.line(
+                X=np.array([e]),
+                Y=np.array([eval_results]),
+                win=win,
+                name='Error',
+                update='append')
+            time.sleep(0.1)
 
-    # while True:
-    #     num = input ("Enter two numbers separated by a comma, to see their product: ")
-    #     try:
-    #         a, b = num.split(',')
-    #     except:
-    #         logging.error('Input format seems to be wrong, please try again.')
-    #         continue
+    while True:
+        num = input ("Enter two numbers separated by a comma, to see their product: ")
+        try:
+            a, b = num.split(',')
+        except:
+            logging.error('Input format seems to be wrong, please try again.')
+            continue
 
-    #     a = int(a)
-    #     b = int(b)
+        a = int(a)
+        b = int(b)
 
-    #     state_index = q_table.get_state_index([a,b])
-    #     action_index = torch.argmax(q_table.q_table[state_index]).item()
-    #     res = q_table.actions[action_index]
-    #     print(f'{a} x {b} = {res}')
+        state_index = q_table.get_state_index([a,b])
+        action_index = torch.argmax(q_table.q_table[state_index]).item()
+        res = q_table.actions[action_index]
+        print(f'{a} x {b} = {res}')
     
 
     
