29470035c75ffc9b486458dabb0bf0ba05fa8af3
meghdadFar <meghdad.farahmand@gmail.com>
2023-05-07 14:51:12 +0200

    Replace visdom with tensorboard

--- kandula/Q_learning.py
+++ kandula/Q_learning.py
@@ -7,6 +7,9 @@ from kandula import logging
 import visdom
 import numpy as np
 import itertools
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter()
 
 
 class Egreedy:
@@ -117,8 +120,7 @@ class QL:
         update_value = q_sa + self.alpha *(reward + self.gamma*q_spap - q_sa)
         self.q_table.q_table[state_index, action_index] = update_value
         return self.q_table
-    
-    
+
     def evaluate_rl_agent(self, get_correct_action):
         elements = [[i for i in range(1, l+1)] for l in self.q_table.state_space]
         all_possible_states = list(itertools.product(*elements))
@@ -131,22 +133,22 @@ class QL:
                 error += 1
         percentage_error = error*100/len(all_possible_states)
         return percentage_error
-    
-    
+
     def train(self, num_epochs, get_correct_action):
-        logging.info('Initializing plot...')
-        viz = visdom.Visdom()
-        win = viz.line(
-            X=np.array([0]), Y=np.array([0]))
         logging.info('Training the model...')
+        # viz = visdom.Visdom()
+        # win = viz.line(
+        #     X=np.array([0]), Y=np.array([0]))
         for e in range(1, num_epochs):
             self.update()
             if e % 1000 == 0:
                 eval_results = self.evaluate_rl_agent(get_correct_action)
-                viz.line(
-                    X=np.array([e]),
-                    Y=np.array([eval_results]),
-                    win=win,
-                    name='Error',
-                    update='append')
+                writer.add_scalar("Error", eval_results, e)
+                writer.flush()
+                # viz.line(
+                #     X=np.array([e]),
+                #     Y=np.array([eval_results]),
+                #     win=win,
+                #     na me='Error',
+                #     update='append')
 
